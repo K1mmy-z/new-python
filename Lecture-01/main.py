a61a5ca5d95cb605f7f60e6fda8b6f4d7eba75ac
K1mmy-z <pinthong8321@gmail.com>
2026-06-30 15:38:10 +0700

    EndLecture-01

---
+++ Lecture-01/main.py
@@ -0,0 +1,8 @@
+print("Hello my Friend")
+print("Information and Network Engineering")
+print("Pongsakoron Pinthong I'm Good :)")
+
+print("i am hungry")
+print('He ask me " Who get the books? "')
+print("I'm good and \"i will do my best\"")
+print('/\\')
